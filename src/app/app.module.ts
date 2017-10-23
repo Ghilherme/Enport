00a6df76eb88e8558fab10c7a94a2af1786a8b45
Guilherme Pereira de Oliveira - GOI <guilherme.oliveira@iteris.local>
2017-10-23 19:27:39 -0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
